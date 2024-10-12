08c61a1429d9186346164e175f822f2b4667dd0e
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2024-10-12 12:28:25 +0300

    Create project architecture

---
+++ BirthdayPresent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirthdayPresent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
