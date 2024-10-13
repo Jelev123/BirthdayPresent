5482aa82f25876a827975f3ef3f94f96cddd02e6
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2024-10-13 09:03:28 +0300

    Service registrator

---
+++ BirthdayPresent/ConfigExtensions/ServiceRegistrator.cs
@@ -0,0 +1,24 @@
+﻿namespace BirthdayPresent.ConfigExtensions
+{
+    using BirthdayPresent.Core.Interfaces.Employee;
+    using BirthdayPresent.Core.Interfaces.Gift;
+    using BirthdayPresent.Core.Services.Employee;
+    using BirthdayPresent.Core.Services.Gift;
+    using BirthdayPresent.Infrastructure.Data;
+    using BirthdayPresent.Infrastructure.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+
+    public static class ServiceRegistrator
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddHttpClient();
+        }
+    }
+}
